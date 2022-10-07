58088aba037d4089f76cde06ee7d7b8292f4eee9
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-10-07 12:12:58 +0700

    update Part#1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </>,
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, The Page You Tried Cannot Be Found</h3>
+        <Link to="/" className="btn">
+          BACK HOME
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
