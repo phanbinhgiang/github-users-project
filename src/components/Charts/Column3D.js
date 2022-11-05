6347deccd1b23966565c2f2c663d8aa5da436b3e
Giang Binh Phan <phan.phan@hitachivantara.com>
2022-11-05 23:07:16 +0700

    update

--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Column3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'column3d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: 'Most Popular',
+        yAxisName: 'Stats',
+        xAxisName: 'Repos',
+        xAxisNameFontSize: '16px',
+        yAxisNameFontSize: '16px',
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
